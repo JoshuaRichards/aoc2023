7eb9bef02a61c2fdd00536731d980b9db42341b6
Joshua Richards <8954297+JoshuaRichards@users.noreply.github.com>
2023-12-09 16:29:01 +1100

    day 9

---
+++ 09/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day9;
+
+public class Program
+{
+    public static void Main()
+    {
+        var input = File.ReadLines("input.txt").Select(line => line.Split(' ').Select(long.Parse).ToArray()).ToArray();
+
+        var part1 = input.Sum(Extrapolate);
+        Console.WriteLine(part1);
+        var part2 = input.Sum(ExtrapolateLeft);
+        Console.WriteLine(part2);
+    }
+
+    public static long Extrapolate(long[] input)
+    {
+        if (input.Distinct().Count() == 1) return input[0];
+
+        var nextLevel = Enumerable.Range(1, input.Length - 1).Select(i => input[i] - input[i - 1]).ToArray();
+        return input.Last() + Extrapolate(nextLevel);
+    }
+
+    public static long ExtrapolateLeft(long[] input)
+    {
+        if (input.Distinct().Count() == 1) return input[0];
+
+        var nextLevel = Enumerable.Range(1, input.Length - 1).Select(i => input[i] - input[i - 1]).ToArray();
+        return input.First() - ExtrapolateLeft(nextLevel);
+    }
+}
